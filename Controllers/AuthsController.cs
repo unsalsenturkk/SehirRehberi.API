877d74105b7164b1c75f029e1bed1a5f254383e8
unsalsenturkk <unsalsenturkk@gmail.com>
2021-12-07 19:10:51 +0300

    AuthController Yazılması - Register

---
+++ Controllers/AuthsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SehirRehberi.API.Data;
+using SehirRehberi.API.Dtos;
+using SehirRehberi.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthsController : Controller
+    {
+        IAuthRepository _authRepository;
+        IConfiguration _configuration;
+        public AuthsController(IAuthRepository authRepository, IConfiguration configuration)
+        {
+            _authRepository = authRepository;
+            _configuration = configuration;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
+        {
+            if (await _authRepository.UserExists(userForRegisterDto.UserName))
+            {
+                ModelState.AddModelError("UserName", "Username already exists");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var userToCreate = new User
+            {
+                UserName = userForRegisterDto.UserName
+            };
+            var createdUser = await _authRepository.Register(userToCreate, userForRegisterDto.Password);
+            return StatusCode(201);
+        }
+
+    }
+}
