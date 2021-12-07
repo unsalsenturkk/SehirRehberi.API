c126a9b36c1ae12e8f478613b64813022319c1d4
unsalsenturkk <unsalsenturkk@gmail.com>
2021-12-07 13:23:08 +0300

    Cities Controller Yazımı ve AutoMapper İhtiyacı

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SehirRehberi.API.Data;
+using SehirRehberi.API.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CitiesController : Controller
+    {
+        IAppRepository _appRepository;
+        public CitiesController(IAppRepository appRepository)
+        {
+            _appRepository = appRepository;
+        }
+
+        public ActionResult GetCities()
+        {
+            var cities = _appRepository.GetCities();
+            var citiesToReturn = _mapper.Map<List<CityForListDto>>(cities);    
+            return Ok(cities);
+        }
+    }
+}
