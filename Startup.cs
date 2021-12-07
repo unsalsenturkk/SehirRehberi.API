13a81b9d2d85a29a2a20bec25ad8ef1adfb7aab4
unsalsenturkk <unsalsenturkk@gmail.com>
2021-12-07 18:28:13 +0300

    JWT Konfigurasyonunun Yapılması

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -6,10 +7,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using SehirRehberi.API.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SehirRehberi.API
@@ -23,9 +26,13 @@ namespace SehirRehberi.API
 
         public IConfiguration Configuration { get; }
 
+       
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var key = Encoding.ASCII.GetBytes(Configuration.GetSection("Appsettings:Token").Value);
+
             //injection alanı -> Hangi nesne için hangi configuretion kullanılacak
             services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddAutoMapper();
@@ -35,6 +42,17 @@ namespace SehirRehberi.API
             });
             services.AddCors();
             services.AddScoped<IAppRepository, AppRepository>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+
+                };
+            });
 
         }
 
@@ -47,7 +65,7 @@ namespace SehirRehberi.API
             }
 
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
-
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
